d1f73c0b78fa4df47ef5e3d3252f04eb0453cc3c
Andrey Onishchenko <andrey.onishchenko@finteractions.com>
2020-04-16 13:14:40 +0300

    Email validator.

--- app/models/tt0413.rb
+++ app/models/tt0413.rb
@@ -2,11 +2,9 @@ class Tt0413 < ApplicationRecord
   # HACK - bad practice
   self.table_name = 'tt-0413'
 
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
   validates :first_name, presence: true, length: { minimum: 2, maximum: 100 }
   validates :last_name, presence: true, length: { minimum: 2, maximum: 100 }
-  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  validates :email, presence: true, email: true
   validates :phone, presence: true, length: { minimum: 3, maximum: 100 }
   validates :company, presence: true, length: { minimum: 2, maximum: 100 }
   validates :job, presence: true, length: { minimum: 2, maximum: 100 }
