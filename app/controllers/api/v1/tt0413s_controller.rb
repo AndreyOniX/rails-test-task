a1ccb2970d7442f0c938cd10ac6e7d66b1560c59
Andrey Onishchenko <andrey.onishchenko@finteractions.com>
2020-04-16 14:24:20 +0300

    API endpoint.

---
+++ app/controllers/api/v1/tt0413s_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    class Tt0413sController < ApiController
+
+      def create
+        tt0413 = Tt0413.new(tt0413_params)
+        if tt0413.save
+          render json: {}, status: :created
+        else
+          render_errors(tt0413, :bad_request)
+        end
+      end
+
+      private
+
+      def tt0413_params
+        params.permit(:first_name, :last_name, :email, :phone, :company, :job, :country)
+      end
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      # HACK - bad practice
+      post '/tt/0413' => 'tt0413s#create'
+    end
+  end
+
 end
