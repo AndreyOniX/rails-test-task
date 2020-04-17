9d8279c7d8d2b9ebd32b22ab61b28e084c39cbbc
Andrey Onishchenko <andrey.onishchenko@finteractions.com>
2020-04-17 14:37:31 +0300

    Route for API doc.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get '/' => redirect('/api_doc/index.html')
+
       post 'authenticate', to: 'authentication#create'
 
       # HACK - bad practice
